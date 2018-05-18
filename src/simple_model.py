2b893b1cf29f42c6dc99a1acbe9745f3d1501b11
Adrian Bratteby <adrian.bratteby@gmail.com>
2018-05-18 13:50:39 +0200

    prova denna

---
+++ src/simple_model.py
@@ -0,0 +1,44 @@
+from keras import optimizers, losses, activations, models
+from keras.layers import Convolution2D, Dense, Input, Flatten, Dropout, MaxPooling2D, BatchNormalization
+from sklearn.model_selection import train_test_split
+import keras
+from keras import sequential
+
+
+input_shape = (99, 81, 1)
+#input_shape = x_train.shape #Lägg på en etta på slutet
+nclass = 12
+#nclass = y_train något
+
+
+#Special weight initialization?
+inp = Input(shape=input_shape)
+norm_inp = BatchNormalization()(inp)
+img_1 = Convolution2D(8, kernel_size=2, activation=activations.relu)(norm_inp)
+img_1 = Convolution2D(8, kernel_size=2, activation=activations.relu)(img_1)
+img_1 = MaxPooling2D(pool_size=(2, 2))(img_1)
+img_1 = Dropout(rate=0.2)(img_1)
+img_1 = Convolution2D(16, kernel_size=3, activation=activations.relu)(img_1)
+img_1 = Convolution2D(16, kernel_size=3, activation=activations.relu)(img_1)
+img_1 = MaxPooling2D(pool_size=(2, 2))(img_1)
+img_1 = Dropout(rate=0.2)(img_1)
+img_1 = Convolution2D(32, kernel_size=3, activation=activations.relu)(img_1)
+img_1 = MaxPooling2D(pool_size=(2, 2))(img_1)
+img_1 = Dropout(rate=0.2)(img_1)
+img_1 = Flatten()(img_1)
+
+dense_1 = BatchNormalization()(Dense(128, activation=activations.relu)(img_1))
+dense_1 = BatchNormalization()(Dense(128, activation=activations.relu)(dense_1))
+dense_1 = Dense(nclass, activation=activations.softmax)(dense_1)
+
+model = models.Model(inputs=inp, outputs=dense_1)
+
+opt = optimizers.Adam()
+
+model.compile(optimizer=opt, loss=losses.binary_crossentropy)
+model.summary()
+
+x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.1, random_state=2017)
+model.fit(x_train, y_train, batch_size=16, validation_data=(x_valid, y_valid), epochs=3, shuffle=True, verbose=2)
+
+model.save(os.path.join(model_path, 'cnn.model'))
